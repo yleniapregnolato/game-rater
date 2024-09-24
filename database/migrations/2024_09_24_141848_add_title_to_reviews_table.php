9bcf9441f791f07443f119aec2a5ee1d53282b4b
yleniapregnolato <yleniapregnolato@gmail.com>
2024-09-24 16:22:38 +0200

    add: title colimn in the review table

---
+++ database/migrations/2024_09_24_141848_add_title_to_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            Schema::table('reviews', function (Blueprint $table) {
+                $table->string('title')->after('id'); 
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            Schema::table('reviews', function (Blueprint $table) {
+                $table->dropColumn('title'); 
+            });
+        });
+    }
+};
