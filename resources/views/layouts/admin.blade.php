bc8cc2efe2e69daa7629f89134c8d044cc59befd
yleniapregnolato <yleniapregnolato@gmail.com>
2024-09-25 17:36:26 +0200

    fix: favicon in app.blade and admin.blade in views/layouts

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,12 +3,16 @@
 
 <head>
     <meta charset="utf-8">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Game-Rater') }}</title>
 
     <!-- Fontawesome 6 cdn -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
@@ -78,7 +82,7 @@
                                     <i class="fa-solid fa-plus"></i> Inserisci recensione
                                 </a>
                             </li>
-                            
+
                         </ul>
 
 
