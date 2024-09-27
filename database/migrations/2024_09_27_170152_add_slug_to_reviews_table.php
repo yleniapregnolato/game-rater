1b5eaefa7ecd71d23978a3304e7eed22dec6204c
yleniapregnolato <yleniapregnolato@gmail.com>
2024-09-27 19:03:33 +0200

    add: slug column in Reviews table

---
+++ database/migrations/2024_09_27_170152_add_slug_to_reviews_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->string('string')->unique()->after('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+    }
+};
