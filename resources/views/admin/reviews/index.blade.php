419d7ded7521392718995a60ff9188722a7d21ab
yleniapregnolato <yleniapregnolato@gmail.com>
2024-09-28 18:26:26 +0200

    feat: card for reviews in views/admin/index.blade

--- resources/views/admin/reviews/index.blade.php
+++ resources/views/admin/reviews/index.blade.php
@@ -3,12 +3,23 @@
 @section('content')
     <h1>Le tue recensioni:</h1>
     {{-- nuova recensione --}}
-    <a class="btn btn-primary" href="{{ route('admin.reviews.create')}}">Inserisci una nuova recensione</a>
+    <a class="btn btn-primary mb-3" href="{{ route('admin.reviews.create') }}">Inserisci una nuova recensione</a>
     {{-- /nuova recensione --}}
 
-    {{-- recensioni caricate --}}
-    <table>
-        
-    </table>
+    <div class="d-flex gap-3">
+        {{-- recensioni caricate --}}
+        @foreach ($reviews as $review)
+            <div class="card mt-3" style="width: 18rem;">
+                <img src="{{ $review->game->image ? asset($review->game->image) : asset('default-image.jpg') }}" class="card-img-top" alt="Immagine di {{ $review->game->name }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $review->title }}</h5>
+                    <p class="card-text">{{ $review->review }}</p>
+                    <a href="#" class="btn btn-primary">Dettagli</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+
     {{-- /recensioni caricate --}}
 @endsection
